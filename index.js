fa5d4ad292ad4a2e6f347f1a56cf9d255b31a861
Jostein24sfj <jostein24sfj@gmail.com>
2024-12-12 14:25:34 +0100

    second commit

--- index.js
+++ index.js
@@ -33,3 +33,28 @@ console.log("The canDoMagic goes into the Boolean Box");
 console.log("The canDoMagic is of type: " + typeof canDoMagic);
 
 let magical = "You have completed this task!";
+
+let magicPower = 7;
+let strength = 5;
+let wisdom = 6;
+
+let lockMagicOpen = magicPower >= 5;
+let lockStrengthOpen = strength >= 4;
+let lockWisdomOpen = wisdom >= 6;
+console.log(lockMagicOpen);
+console.log(lockStrengthOpen);
+console.log(lockWisdomOpen);
+
+/* Magic Lock requires 5 magic power to unlock
+Strength Lock requires 4 strength to unlock
+Wisdom Lock requires 6 wisdom to unlock
+Set each lock to true if the hero can open it.
+If all locks are opened, the hero can enter. */
+
+if (lockMagicOpen && lockStrengthOpen && lockWisdomOpen) {
+  console.log("All locks are open! You enter the magical chamber.");
+} else if (magicPower === 4 || strength === 3 || wisdom === 5) {
+  console.log("Could not open all locks. Lacking attribute");
+} else {
+  console.log("You couldn't open all the locks. Keep trying!");
+}
