c4b67f27bffeee01c7f200e004bdc7fafeebe716
Jostein24sfj <jostein24sfj@gmail.com>
2024-12-12 10:50:52 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+let roseEssence;
+let dragonTears;
+let unicornDust;
+let liquifiedPrincess;
+
+let lovePotion = roseEssence + dragonTears + unicornDust + liquifiedPrincess;
+
+let wizardName = "Merlin";
+let wizardAge = 100;
+let isWizardPowerful = true;
+let apprenticeName = "You";
+let numberOfSpells;
+let canDoMagic = false;
+
+console.log("The love potion contains: " + lovePotion);
+
+console.log("The wizardName goes into the String Sack!");
+console.log("The wizardName is of type: " + typeof wizardName);
+
+console.log("The wizardAge goes into the Number Chest!");
+console.log("The wizardAge is of type: " + typeof wizardAge);
+
+console.log("The isWizardPowerful goes into the Boolean Box!");
+console.log("The isWizardPowerful is of type: " + typeof isWizardPowerful);
+
+console.log("The apprenticeName goes into the String Sack!");
+console.log("The apprenticeName is of type: " + typeof apprenticeName);
+
+console.log("The numberOfSpells goes into the Number Chest!");
+console.log("The numberOfSpells is of type: " + typeof numberOfSpells);
+
+console.log("The canDoMagic goes into the Boolean Box");
+console.log("The canDoMagic is of type: " + typeof canDoMagic);
+
+let magical = "You have completed this task!";
